b81943233b9800fc3665156b6063bef5c54af852
mnkhod <mnkhod.dev@gmail.com>
2022-01-31 15:49:47 +0800

    Fixed Alias On Vite Config For Preact Compat

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  alias: {
-    react: 'preact/compat',
-    'react-dom': 'preact/compat'
-  }
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat'
+    }
+  },
 })
